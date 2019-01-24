112bab4023c419b83dde57771c8fe52159d6bee0
mohmah7 <mohmah7@users.noreply.github.com>
2019-01-24 09:10:08 +0200

    Update assignment.sh

--- assignment.sh
+++ assignment.sh
@@ -3,9 +3,26 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+    
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+
+sudo apt-get install docker-ce
+
+#sudo amazon-linux-extras install docker•udo service docker start
+#sudo usermod -a -G docker ec2-user
 
 #cd  assignments/Assignments
 
