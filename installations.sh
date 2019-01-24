faa80a62c2a7277d98570baab492a5bca6c11af4
mohmah7 <mohmah7@users.noreply.github.com>
2019-01-24 19:55:16 +0200

    Create installations.sh

---
+++ installations.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get upgrade -y
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+ 
+apt-get install curl -y
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+
+apt-get install docker-ce
