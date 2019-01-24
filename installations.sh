76287394fb987d62378b6d8462b004be4fb09d35
mohmah7 <mohmah7@users.noreply.github.com>
2019-01-24 20:36:34 +0200

    Update installations.sh

--- installations.sh
+++ installations.sh
@@ -12,7 +12,7 @@ apt-get install \
  
 apt-get install curl -y
     
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  apt-key add -
 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
