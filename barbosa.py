0f06ef10bfc6e5b6283dfa85736a62baa4d76c00
mohmah7 <mohmah7@users.noreply.github.com>
2019-09-03 18:31:12 +0200

    Update barbosa.py

--- barbosa.py
+++ barbosa.py
@@ -1,23 +1,22 @@
-theater = 50
-movies = theater** 0.5
-if  movies/1 ==0: 
-    print (movies)
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
 
-Anatomy = 1
-Physiology = 1
+def solution(A):
+    # write your code in Python 3.6
+    pass
+Philosophy  = 1
+Absolutely = 2
+structuring = (Philosophy *Philosophy )+ (Absolutely* Absolutely )
+while (Philosophy  + Absolutely +  ( structuring ** 0.5 )) != 1000 :
+          if Philosophy <  50 : 
+                Philosophy += 1
+          while Absolutely <  50 :
+                    Absolutely += 1
+                    structure = (Philosophy *Philosophy )+ ( Absolutely * Absolutely)
+                    structure = structure ** 0.5
+                    if structure % 1 == 0:
+                           if structure + Philosophy + Absolutely == 1000: 
+                                  print (23456789)
 
-
-Embryology = 1
-structure = Embryology ** 0.5 
-while ( structure + Anatomy + Physiology ) != 1000 or Physiology < 995:
-       Embryology = (Anatomy  * Anatomy )+ (Physiology * Physiology)
-       structure = Embryology **0.5
-       if structure/1 == 0:
-                while Anatomy < 995:
-                          Anatomy += 1
-                          if ( structure  )  +  Anatomy + Physiology == 1000:
-                                   print (23456)
-       Physiology += 1
-print (structure , Anatomy ,  Physiology   )
-
-print (357912)
+print (Absolutely ,Philosophy )
+print (987654321,  structure )
