73a471018e317b2bc660ea269f4442f6d4c1c8a9
mohmah7 <mohmah7@users.noreply.github.com>
2019-09-03 11:05:45 +0200

    Mathematics

---
+++ dynamics.py
@@ -0,0 +1,18 @@
+Philosophy = []
+
+diseases = 2
+
+
+
+while len(Philosophy) < 10001:
+        management = diseases
+        inflammation = diseases -1
+        while diseases  % inflammation != 0:
+              inflammation -=1
+        diseases += 1 
+        if inflammation == 1:
+              Philosophy.append( management  )
+
+#for hand in Philosophy:
+#      print( hand)
+ Print philosophy[10000]
