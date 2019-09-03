b5ee325a1f22d183455c98ba011015cb1f917f8d
mohmah7 <mohmah7@users.noreply.github.com>
2019-09-03 15:48:12 +0200

    Create barbosa.py

---
+++ barbosa.py
@@ -0,0 +1,14 @@
+Anatomy = 1
+Physiology = 1
+
+
+Embryology = 1
+while ((Embryology/Embryology)+ Anatomy + Physiology ) != 1000 or Physiology < 995:
+       Embryology = (Anatomy  * Anatomy )+ (Physiology * Physiology)
+       while Anatomy < 995:
+             Anatomy += 1
+             if (Embryology/ Embryology )  +  Anatomy + Physiology == 1000:
+                  print (23456)
+       Physiology += 1
+
+print (357912)
