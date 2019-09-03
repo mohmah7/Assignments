f81909bbb5bedf243d6de8334199353dbe46beb4
mohmah7 <mohmah7@users.noreply.github.com>
2019-09-03 17:14:06 +0200

    Update barbosa.py

--- barbosa.py
+++ barbosa.py
@@ -1,14 +1,23 @@
+theater = 50
+movies = theater** 0.5
+if  movies/1 ==0: 
+    print (movies)
+
 Anatomy = 1
 Physiology = 1
 
 
 Embryology = 1
-while ((Embryology/Embryology)+ Anatomy + Physiology ) != 1000 or Physiology < 995:
+structure = Embryology ** 0.5 
+while ( structure + Anatomy + Physiology ) != 1000 or Physiology < 995:
        Embryology = (Anatomy  * Anatomy )+ (Physiology * Physiology)
-       while Anatomy < 995:
-             Anatomy += 1
-             if (Embryology/ Embryology )  +  Anatomy + Physiology == 1000:
-                  print (23456)
+       structure = Embryology **0.5
+       if structure/1 == 0:
+                while Anatomy < 995:
+                          Anatomy += 1
+                          if ( structure  )  +  Anatomy + Physiology == 1000:
+                                   print (23456)
        Physiology += 1
+print (structure , Anatomy ,  Physiology   )
 
 print (357912)
