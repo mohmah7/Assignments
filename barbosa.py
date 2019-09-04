87b6dd21749e06ed75bb72c171d18408a5c51239
mohmah7 <mohmah7@users.noreply.github.com>
2019-09-04 07:29:58 +0200

    Update barbosa.py

--- barbosa.py
+++ barbosa.py
@@ -10,6 +10,7 @@ structuring = (Philosophy *Philosophy )+ (Absolutely* Absolutely )
 while (Philosophy  + Absolutely +  ( structuring ** 0.5 )) != 1000 :
           if Philosophy <  50 : 
                 Philosophy += 1
+                Absolutely = Philosophy +1 
           while Absolutely <  50 :
                     Absolutely += 1
                     structure = (Philosophy *Philosophy )+ ( Absolutely * Absolutely)
@@ -17,6 +18,6 @@ while (Philosophy  + Absolutely +  ( structuring ** 0.5 )) != 1000 :
                     if structure % 1 == 0:
                            if structure + Philosophy + Absolutely == 1000: 
                                   print (23456789)
-
+          #Absolutely = Philosophy +1 
 print (Absolutely ,Philosophy )
 print (987654321,  structure )
