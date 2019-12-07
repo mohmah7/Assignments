e6a1dd50ffbc871d3ef5ae47c0329b87b30fa81f
mohmah7 <mohmah7@users.noreply.github.com>
2019-12-07 07:18:40 +0200

    Update game.py

--- game.py
+++ game.py
@@ -139,6 +139,30 @@ is printed """
          #print(" %i " % i, end=" ")
       print()
 
+def index():
+    #ars = """%s%s"""% ( a,b)
+    ars = """
+<html><head>
+<title>Formular</title>
+</head>
+<body>
+<P>%s</P>
+<FORM value="form" action="get_info" method="post">
+  <P>
+	<LABEL for="Blacks">Blacks: </LABEL>
+	<INPUT type="text" name="Blacks"><BR>
+	<LABEL for="Whites">White: </LABEL>
+	<INPUT type="text" name="Whites"><BR>
+	<INPUT type="submit" value="Send"> <INPUT type="reset">
+  </P>
+</FORM>
+</body>
+</html>
+""" % (show_current_guess(new_guess))
+    return ars 
+
+
+
 if __name__ == "__main__":
    colours = ["red","green","blue","yellow","orange","pink"]
    guesses = []				
